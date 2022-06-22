5839a190c7544223116891dea8582ec538ca7a96
Brian Taguba <briantaguba05@gmail.com>
2022-06-22 21:05:40 +0800

    google map fix

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api'
 import themeConfig from 'src/configs/themeConfig'
-import { firebaseAuth, firestore } from 'src/configs/firebase';
-import { collection, getDocs, query, where, getDoc, doc } from "firebase/firestore";
+import { firebaseAuth, firestore } from 'src/configs/firebase'
+import { collection, getDocs, query, where, getDoc, doc } from 'firebase/firestore'
 
 // ** MUI Imports
 import Grid from '@mui/material/Grid'
@@ -13,29 +13,32 @@ import { useTheme } from '@mui/material/styles'
 
 function Dashboard() {
   const [values, setValues] = useState({
-    locationPositions: null,
+    locationPositions: null
   })
 
   const theme = useTheme()
 
   useEffect(() => {
     getUserdata()
-  }, []);
+  }, [])
 
   async function getUserdata() {
-    const q = query(collection(firestore, "users"), where("auth_uid", "==", firebaseAuth.currentUser.uid))
+    const q = query(collection(firestore, 'users'), where('auth_uid', '==', firebaseAuth.currentUser.uid))
 
-    await getDocs(q).then((snapshot) => {
-      snapshot.forEach(async (value) => {
+    await getDocs(q).then(snapshot => {
+      snapshot.forEach(async value => {
         if (value.data().role == 'family') {
-          await getDoc(doc(firestore, "users", value.data().riderID)).then((data) => {
+          await getDoc(doc(firestore, 'users', value.data().riderID)).then(data => {
             if (data.exists()) {
               setValues(prevValues => {
                 return {
-                  ...prevValues, locationPositions: [{
-                    lat: data.data().location.latitude,
-                    lng: data.data().location.longitude,
-                  }]
+                  ...prevValues,
+                  locationPositions: [
+                    {
+                      lat: data.data().location.latitude,
+                      lng: data.data().location.longitude
+                    }
+                  ]
                 }
               })
             }
@@ -46,64 +49,68 @@ function Dashboard() {
   }
 
   const mapCenter = {
-    lat: 16.4082762,
-    lng: 120.6132623
+    lat: 14.5995,
+    lng: 120.9842
   }
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyBB34ej_ByQorE7J9Fb_YHzHYCgwgfscEU",
+    googleMapsApiKey: 'AIzaSyBB34ej_ByQorE7J9Fb_YHzHYCgwgfscEU'
   })
 
   const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds(mapCenter);
-    map.fitBounds(bounds);
-    setValues(prevValues => { return { ...prevValues, map: map } })
+    const bounds = new window.google.maps.LatLngBounds(mapCenter)
+    map.setCenter(bounds)
+
+    //map.fitBounds(bounds)
+    setValues(prevValues => {
+      return { ...prevValues, map: map }
+    })
   }, [])
 
   const onUnmount = React.useCallback(function callback(map) {
-    setValues(prevValues => { return { ...prevValues, map: null } })
+    setValues(prevValues => {
+      return { ...prevValues, map: null }
+    })
   }, [])
 
   return (
     <Grid container spacing={6}>
       <Head>
         <title>{`${themeConfig.templateName} - Home`}</title>
-        <meta
-          name='description'
-          content={`${themeConfig.templateName} - Home`}
-        />
+        <meta name='description' content={`${themeConfig.templateName} - Home`} />
         <meta name='keywords' content='RSOS' />
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
       <Grid item xs={12} md={12} lg={12}>
         <Card>
-          {
-            isLoaded ? <GoogleMap
+          {isLoaded ? (
+            <GoogleMap
               mapContainerStyle={{
                 height: '600px'
               }}
               center={mapCenter}
-              zoom={5}
+              zoom={10}
               onLoad={onLoad}
               onUnmount={onUnmount}
             >
-              {
-                values.locationPositions != null ?
-                  values.locationPositions.map((data, index) => {
-                    return <Marker
-                      key={`marker-${index}`}
-                      position={{
-                        lat: data.lat,
-                        lng: data.lng
-                      }}
-                    />
-                  }) : null
-              }
-            </GoogleMap> : <CircularProgress />
-          }
-
-
+              {values.locationPositions != null
+                ? values.locationPositions.map((data, index) => {
+                    return (
+                      <Marker
+                        key={`marker-${index}`}
+                        position={{
+                          lat: data.lat,
+                          lng: data.lng
+                        }}
+                      />
+                    )
+                  })
+                : null}
+            </GoogleMap>
+          ) : (
+            <CircularProgress />
+          )}
         </Card>
       </Grid>
     </Grid>
