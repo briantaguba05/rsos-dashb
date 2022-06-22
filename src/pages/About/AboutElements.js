e8a6a73c8b4e8911537129f406fb366f493666d0
Brian Taguba <briantaguba05@gmail.com>
2022-06-22 17:56:10 +0800

    commmss

---
+++ src/pages/About/AboutElements.js
@@ -0,0 +1,165 @@
+import styled from 'styled-components'
+import { Link as LinkR } from 'react-router-dom'
+
+export const AboutMain = styled.div`
+  @media screen and (max-width: 768px) {
+    text-align: center;
+  }
+`
+
+export const AboutContent = styled.div`
+  background: #14213d;
+  padding: 20px 30px;
+  height: 800px;
+  position: left;
+  z-index: 1;
+
+  @media screen and (max-width: 768px) {
+    height: 1000px;
+  }
+
+  @media screen and (max-width: 480px) {
+    height: 1500px;
+  }
+`
+
+export const AboutH1 = styled.h1`
+  color: #fff;
+  font-size: 48px;
+  text-align: center;
+  padding: 10px 10px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 40px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 30px;
+  }
+`
+
+export const AboutContentSec = styled.div`
+  background: #fff;
+  padding: 20 30px;
+  height: 800px;
+  position: left;
+  z-index: 1;
+
+  @media screen and (max-width: 768px) {
+    height: 1000px;
+  }
+
+  @media screen and (max-width: 480px) {
+    height: 1500px;
+  }
+`
+
+export const AboutH1Sec = styled.h1`
+  color: #14213d;
+  font-size: 48px;
+  text-align: center;
+  padding: 10px 10px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 40px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 30px;
+  }
+`
+
+export const BackButton = styled(LinkR)`
+  box-shadow: 0px 0px 0px 2px #9fb4f2;
+  justify-content: center;
+  background-color: #14213d;
+  border-radius: 10px;
+  border: 1px solid #4e6096;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  padding: 12px 37px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #283966;
+  transition: all 0.2s ease-in-out;
+  font-family: 'Roboto Mono', monospace;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #14213d;
+  }
+`
+
+export const Filler = styled.div`
+  height: 80px;
+  color: fff;
+`
+
+export const AboutP = styled.p`
+  color: #fff;
+  padding: 10px 10px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 12px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 8px;
+  }
+`
+
+export const AboutPSec = styled.p`
+  color: #14213d;
+  padding: 10px 10px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 12px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 8px;
+  }
+`
+
+export const AboutIMG = styled.img`
+  height: 300px;
+  float: right;
+  padding: 10px 10px 10px 10px;
+`
+
+export const AboutIMGSec = styled.img`
+  height: 300px;
+  margin-right: 30px;
+`
+
+export const SectionOne = styled.div`
+  text-align: right;
+  height: 100px;
+
+  justify-content: flex-start;
+`
+
+export const AboutPLarge = styled.p`
+  color: #fff;
+  padding: 10px 10px;
+  font-size: 30px;
+`
+
+export const AboutPSecLarge = styled.p`
+  color: #14213d;
+  padding: 10px 10px;
+  font-size: 30px;
+`
+
+export const AboutPSecMed = styled.p`
+  color: #14213d;
+  padding: 10px 10px;
+  font-size: 25px;
+`
+
+export const SectionTwo = styled.div`
+  text-align: left;
+  justify-content: flex-start;
+  height: 300px;
+`
--- src/pages/tutorials/TutorialsElements.js
+++ src/pages/tutorials/TutorialsElements.js
@@ -1,9 +1,9 @@
-import styled from "styled-components";
-import { Link as LinkR } from "react-router-dom";
+import styled from 'styled-components'
+import { Link as LinkR } from 'react-router-dom'
 // eslint-disable-next-line no-unused-vars
-import { Link as LinkS } from "react-scroll";
+import { Link as LinkS } from 'react-scroll'
 // eslint-disable-next-line no-unused-vars
-const goodblue = "#2293D4";
+const goodblue = '#2293D4'
 
 export const TutorialsContainer = styled.div`
   height: 800px;
@@ -20,7 +20,7 @@ export const TutorialsContainer = styled.div`
   @media screen and (max-width: 480px) {
     height: 1300px;
   }
-`;
+`
 
 export const TutorialsWrapper = styled.div`
   max-width: 1500px;
@@ -39,7 +39,7 @@ export const TutorialsWrapper = styled.div`
     grid-template-columns: 1fr;
     padding: 0 20px;
   }
-`;
+`
 
 export const TutorialsCard = styled.div`
   background: #fff;
@@ -58,41 +58,42 @@ export const TutorialsCard = styled.div`
     transition: all 0.2s ease-in-out;
     cursor: pointer;
   }
-`;
+`
 
 export const TutorialsIcon = styled.img`
   height: 160px;
   width: 160px;
   margin-bottom: 10px;
-`;
+`
 
 export const TutorialsH1 = styled.h1`
   font-size: 2.5rem;
   color: #fff;
   margin-bottom: 64px;
-  font-family: "Roboto Mono", monospace;
+  font-family: 'Roboto Mono', monospace;
 
   @media screen and (max-width: 480px) {
     font-size: 2rem;
   }
-`;
+`
 
 export const TutorialsH2 = styled.h2`
   font-size: 1rem;
   margin-bottom: 10px;
-  font-family: "Roboto Mono", monospace;
-`;
+  font-family: 'Roboto Mono', monospace;
+`
 
 export const TutorialsP = styled.p`
   font-size: 1rem;
   text-align: center;
-  font-family: "Roboto Mono", monospace;
-`;
+  font-family: 'Roboto Mono', monospace;
+`
+
 export const TutorialsP2 = styled.p`
   font-size: 1rem;
   text-align: left;
-  font-family: "Roboto Mono", monospace;
-`;
+  font-family: 'Roboto Mono', monospace;
+`
 
 export const TutLink = styled(LinkR)`
   white-space: nowrap;
@@ -106,11 +107,11 @@ export const TutLink = styled(LinkR)`
   cursor: pointer;
   transition: all 0.2s ease-in-out;
   text-decoration: none;
-  font-family: "Montserrat", sans-serif;
-`;
+  font-family: 'Montserrat', sans-serif;
+`
 
 export const TitleDiv = styled.div`
-  font-family: "Roboto Mono", monospace;
+  font-family: 'Roboto Mono', monospace;
   font-size: 3rem;
   text-align: center;
   justify-content: center;
@@ -118,24 +119,24 @@ export const TitleDiv = styled.div`
   padding-right: 100px;
   padding-bottom: 100px;
   margin-top: 50px;
-`;
+`
 
 export const ImageCont = styled.img`
   height: auto;
   max-width: 100%;
-`;
+`
 
 export const UnivList = styled.ul`
-  font-family: "Roboto Mono", monospace;
+  font-family: 'Roboto Mono', monospace;
   text-align: center;
   padding: 15px;
-`;
+`
 
 export const LocList = styled.li`
-  font-family: "Roboto Mono", monospace;
+  font-family: 'Roboto Mono', monospace;
   text-align: left;
   padding: 15px;
-`;
+`
 
 export const TutorialsCard2 = styled.div`
   background: #fff3;
@@ -150,7 +151,7 @@ export const TutorialsCard2 = styled.div`
   padding-bottom: 30px;
   box-shadow: 0 1 px 3px rgba(0, 0, 0, 0.2);
   transition: all 0.2s ease-in-out;
-`;
+`
 
 export const TutorialsWrapper2 = styled.div`
   max-width: 1500px;
@@ -169,4 +170,4 @@ export const TutorialsWrapper2 = styled.div`
     grid-template-columns: 1fr;
     padding: 0 20px;
   }
-`;
+`
